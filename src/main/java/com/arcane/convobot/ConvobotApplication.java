dbadae48e2cae7a0ba8dfb5466d7c24f3c46bae3
Mahin <mahin.inovace@gmail.com>
2023-09-02 05:38:37 +0600

    Initial commit

---
+++ src/main/java/com/arcane/convobot/ConvobotApplication.java
@@ -0,0 +1,13 @@
+package com.arcane.convobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arcane/convobot/ConvobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcane.convobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
