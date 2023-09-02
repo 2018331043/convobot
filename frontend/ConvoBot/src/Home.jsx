104573799ba284aadcf3a3a16391b1e99fc5bffa
nightmare117 <iftekherfuad@gmail.com>
2023-09-03 05:52:34 +0600

    frontend setup add

---
+++ frontend/ConvoBot/src/Home.jsx
@@ -0,0 +1,10 @@
+import Typography from '@mui/material/Typography';
+import './styling/Home.css'
+
+export default function Home(){
+    return (
+        <>
+            <Typography className='hello1'>hello!</Typography>
+        </>
+    )
+}
