b405203cc902ac2241fb859fb5c08f334ef0c13c
nightmare117 <iftekherfuad@gmail.com>
2023-09-03 10:29:36 +0600

    Home page update

--- frontend/ConvoBot/src/Home.jsx
+++ frontend/ConvoBot/src/Home.jsx
@@ -1,10 +1,32 @@
 import Typography from '@mui/material/Typography';
+import logo from '../src/assets/bot.png'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box'
 import './styling/Home.css'
 
 export default function Home(){
     return (
         <>
-            <Typography className='hello1'>hello!</Typography>
+            <div className='home-body'>
+                <div className='home-body-first'>
+                    <div className='home-body-first-left'>
+                        <img src={logo} alt='logo'/>
+                        <Typography className='home-body-first-left-name'>Convo<span className='home-span-1'>Bot</span></Typography>
+                    </div>
+                    <div className='home-body-first-right'>
+                        <Button variant="outlined" className='home-body-first-right-button-1'>Sign-In</Button>
+                        <Button variant="text" className='home-body-first-right-button-1'>Register</Button>
+                    </div>
+                </div>
+                <div className='home-body-second'>
+                    <div  className='home-body-second-left'>
+                    <Typography className='home-body-second-left-title'>Elevate Business Operations with <span className='home-span-1'>AI Chat</span></Typography>
+                    </div>
+                    <div className='home-body-second-right'>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
