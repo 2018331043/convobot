9c0713d09b38cae80d4a40581e950b87a5ae4e5c
Mahin <mahin.inovace@gmail.com>
2023-09-04 11:08:49 +0600

    ADDED ASCII PATTERNS

--- src/main/java/com/arcane/convobot/ConvobotApplication.java
+++ src/main/java/com/arcane/convobot/ConvobotApplication.java
@@ -1,5 +1,7 @@
 package com.arcane.convobot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -7,8 +9,19 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class ConvobotApplication {
+	private static final Logger log = LoggerFactory.getLogger(ConvobotApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ConvobotApplication.class, args);
+		String pattern =
+						"  /$$$$$$                                                /$$$$$$$\n" +
+						" /$$__  $$                                              | $$__  $$\n" +
+						"| $$  \\__/  /$$$$$$  /$$$$$$$  /$$    /$$ /$$$$$$       | $$  \\ $$  /$$$$$$  /$$$$$$\n" +
+						"| $$       /$$__  $$| $$__  $$|  $$  /$$//$$__  $$      | $$$$$$$  /$$__  $$|_  $$_/\n" +
+						"| $$      | $$  \\ $$| $$  \\ $$ \\  $$/$$/| $$  \\ $$      | $$__  $$| $$  \\ $$  | $$\n" +
+						"| $$    $$| $$  | $$| $$  | $$  \\  $$$/ | $$  | $$      | $$  \\ $$| $$  | $$  | $$ /$$\n" +
+						"|  $$$$$$/|  $$$$$$/| $$  | $$   \\  $/  |  $$$$$$/      | $$$$$$$/|  $$$$$$/  |  $$$$/\n" +
+						" \\______/  \\______/ |__/  |__/    \\_/    \\______/       |_______/  \\______/    \\___/\n";
+		System.out.println(pattern);
 	}
 
 }
