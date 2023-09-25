6d1b6d8c98937d9e30280ec9a5fdb214459fc4ee
Mahin <mahin.inovace@gmail.com>
2023-09-25 09:15:08 +0600

    code refactored

--- src/main/java/com/arcane/convobot/service/WebCrawlerService.java
+++ src/main/java/com/arcane/convobot/service/WebCrawlerService.java
@@ -15,7 +15,8 @@ public class WebCrawlerService {
         String pageContent = "";
         try {
             Document document = Jsoup.connect(websiteURL)
-                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                            "(KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
                     .header("Accept-Language", "*")
                     .get();
             pageContent = document.text(); // Extract all text from the HTML
