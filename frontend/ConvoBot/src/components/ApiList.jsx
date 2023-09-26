f5d7e318adc7d8342be9c539feac225f49b9e891
nightmare117 <iftekherfuad@gmail.com>
2023-09-27 04:28:59 +0600

    api name integration

--- frontend/ConvoBot/src/components/ApiList.jsx
+++ frontend/ConvoBot/src/components/ApiList.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
-import { Button, List, ListItem, ListItemText, Snackbar } from '@mui/material';
+import { Button, Snackbar, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography, Paper } from '@mui/material';
 import { FileCopy } from '@mui/icons-material';
-import { ThemeProvider, createTheme } from '@mui/material';
 import ApiIcon from '@mui/icons-material/Api';
-import Tooltip from '@mui/material/Tooltip';
-import '../styling/components/ApiList.css'
 
 const TextListWithCopy = ({ listItems }) => {
   const [isSnackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
-
 
   const handleCopyClick = (text) => {
     navigator.clipboard.writeText(text);
@@ -23,28 +19,46 @@ const TextListWithCopy = ({ listItems }) => {
 
   return (
     <div>
-      <List>
-        {listItems.map((item) => (
-          <ListItem className='api-list-body' sx={{padding:'10px',marginBottom:'10px'}} key={item.id}>
-            <ApiIcon sx={{marginRight:'15px',color:'rgb(255, 189, 6)'}}/>
-            <ListItemText primary={item.value} />
-            <Tooltip title="Copy">
-            <Button sx={{display:'flex',justifyContent:'center',alignItems:'center'}}
-              variant="text"
-              color="primary"
-              startIcon={<FileCopy sx={{color:'rgba(0,0,0,.6)'}} />}
-              onClick={() => handleCopyClick(item.value)}
-            >
-            </Button>
-            </Tooltip>
-          </ListItem>
-        ))}
-      </List>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><Typography variant="h6">API Name</Typography></TableCell>
+              <TableCell><Typography variant="h6">Key</Typography></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {listItems.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <ApiIcon sx={{ marginRight: '10px', color: 'rgb(255, 189, 6)' }} />
+                    {item.name}
+                  </div>
+                </TableCell>
+                <TableCell>{item.value}</TableCell>
+                <TableCell>
+                  <Tooltip title="Copy">
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      startIcon={<FileCopy sx={{ color: 'rgba(0,0,0,.6)',marginLeft:'12px' }} />}
+                      onClick={() => handleCopyClick(item.value)}
+                    ></Button>
+                  </Tooltip>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <Snackbar
         open={isSnackbarOpen}
         autoHideDuration={2000}
         onClose={handleSnackbarClose}
         message={snackbarMessage}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
       />
     </div>
   );
